c8b7873400ca4c0e3270c00ebf9b9e5d5f3ca7cb
Alex Fernández <alex.fernandez@logtrust.com>
2019-03-08 15:11:33 +0100

    New solution.

---
+++ src/grep.go
@@ -0,0 +1,33 @@
+package main
+
+/**
+ * Show all lines of a file that contain the filter.
+ * Example: go run src/grep.go --filename LICENSE --filter a
+ */
+
+import "log"
+import "fmt"
+import "strings"
+import "flag"
+import "io/ioutil"
+
+func main() {
+	filename := flag.String("filename", "", "filename to grep")
+	filter := flag.String("filter", "", "filter for lines")
+	flag.Parse()
+	//log.Printf("Opening %v filter %v", *filename, *filter)
+	readAndPrint(filename, filter)
+}
+
+func readAndPrint(filename *string, filter *string) {
+	contents, err := ioutil.ReadFile(*filename)
+	if err != nil {
+		log.Fatalf("Error %v", err)
+	}
+	lines := strings.Split(string(contents), "\n")
+	for i := 0; i < len(lines); i++ {
+		if strings.Contains(lines[i], *filter) {
+			fmt.Printf("%v", lines[i]+"\n")
+		}
+	}
+}
